2b0ad1f5822b20457693d6c3aff3dcc9945288e4
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-12-17 16:36:59 -0600

    Building the Backend

---
+++ clone-server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express(); 
+const cors = require("cors"); 
+const bodyParser = require("body-parser"); 
+
+const PORT = 8081;
+
+app.use(cors()); 
+app.use(bodyParser.json()); 
+
+//all routes here
+
+app.use(function(req, res, next){
+    let error = new Error("Not Found")
+    error.status = 404; 
+    next(err); 
+});
+
+app.listen(PORT, function(){
+    console.log(`Server is starting on port${PORT}`); 
+})
