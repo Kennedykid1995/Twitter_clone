93a98f5a844a5431d3711848f1a155403e377129
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-12-17 19:01:44 -0600

    working on back end

--- clone-server/handlers/auth.js
+++ clone-server/handlers/auth.js
@@ -1,3 +1,38 @@
 const db = require("../models/index");
 const jwt = require("jsonwebtoken"); 
- 
+
+exports.signin = function(){
+
+}
+exports.signup = async function(req, res, next){
+    try{
+        //create a user
+        //create a token (signing a token)
+            //process .env.SECRET_KEY
+        let user = await db.User.create(req.body); 
+        let {id, username, profileImageUrl} = user;
+        let token = jwt.sign({
+            id,
+            username, 
+            profileImageUrl
+        }, process.env.SECRET_KEY);
+        return res.status(200).json({
+            id, 
+            usename, 
+            profileImageUrl,
+            token,
+        }); 
+    }catch(err){
+        //see what kind of error 
+        //if it is a certain error
+        //respond with username/already taken
+        //otherwise just send back a generic 400
+        if(err.code === 11000){
+            err.message = "Sorry that username and/ or email is taken."
+        }
+        return next({
+            status: 400,
+            message: err.message,
+        })
+    }
+}
--- clone-server/index.js
+++ clone-server/index.js
@@ -1,15 +1,17 @@
+require("dotenv").config(); 
 const express = require("express");
 const app = express(); 
 const cors = require("cors"); 
 const bodyParser = require("body-parser"); 
 const errorHandler = require("./handlers/error");
-
+const authRoutes = require("./routes/auth");
 
 const PORT = 8081;
 
 app.use(cors()); 
 app.use(bodyParser.json()); 
 
+app.use("/api/auth", authRoutes);
 //all routes here
 
 app.use(function(req, res, next){
@@ -19,6 +21,7 @@ app.use(function(req, res, next){
 });
 
 app.use(errorHandler); 
+
 
 app.listen(PORT, function(){
     console.log(`Server is starting on port${PORT}`); 
